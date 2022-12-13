3351133b0d3d4c02471c904540d80a00848003f0
Neutx <dlprwz@gmail.com>
2022-12-13 15:36:16 +0530

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,6 @@
 'use strict';
 
-
-
-/**
- * NAVBAR TOGGLE FOR MOBILE
- */
+// Navbar for mobile
 
 const navbar = document.querySelector("[data-navbar]");
 const navToggler = document.querySelector("[data-nav-toggler]");
@@ -14,12 +10,7 @@ navToggler.addEventListener("click", function () {
   this.classList.toggle("active");
 });
 
-
-
-/**
- * HEADER & BACK TOP BTN
- * header and back top btn visible when window scroll down to 200px
- */
+// Header and back to top button
 
 const header = document.querySelector("[data-header]");
 const backTopBtn = document.querySelector("[data-back-top-btn]");
@@ -36,118 +27,3 @@ const activeElementOnScroll = function () {
 
 window.addEventListener("scroll", activeElementOnScroll);
 
-
-
-/**
- * SLIDER
- */
-
-const sliders = document.querySelectorAll("[data-slider]");
-
-const sliderInit = function (currentSlider) {
-
-  const sliderContainer = currentSlider.querySelector("[data-slider-container]");
-  const sliderPrevBtn = currentSlider.querySelector("[data-slider-prev]");
-  const sliderNextBtn = currentSlider.querySelector("[data-slider-next]");
-
-  const totalSliderVisibleItems = Number(getComputedStyle(currentSlider).getPropertyValue("--slider-item"));
-  const totalSliderItems = sliderContainer.childElementCount - totalSliderVisibleItems;
-
-  let currentSlidePos = 0;
-
-  const moveSliderItem = function () {
-    sliderContainer.style.transform = `translateX(-${sliderContainer.children[currentSlidePos].offsetLeft}px)`;
-  }
-
-  /**
-   * NEXT SLIDE
-   */
-  const slideNext = function () {
-    const slideEnd = currentSlidePos >= totalSliderItems;
-
-    if (slideEnd) {
-      currentSlidePos = 0;
-    } else {
-      currentSlidePos++;
-    }
-
-    moveSliderItem();
-  }
-
-  sliderNextBtn.addEventListener("click", slideNext);
-
-  /**
-   * PREVIOUS SLIDE
-   */
-  const slidePrev = function () {
-    if (currentSlidePos <= 0) {
-      currentSlidePos = totalSliderItems;
-    } else {
-      currentSlidePos--;
-    }
-
-    moveSliderItem();
-  }
-
-  sliderPrevBtn.addEventListener("click", slidePrev);
-
-  const dontHaveExtraItem = totalSliderItems <= 0;
-  if (dontHaveExtraItem) {
-    sliderNextBtn.setAttribute("disabled", "");
-    sliderPrevBtn.setAttribute("disabled", "");
-  }
-
-  /**
-   * AUTO SLIDE
-   */
-
-  let autoSlideInterval;
-
-  const startAutoSlide = () => autoSlideInterval = setInterval(slideNext, 3000);
-  startAutoSlide();
-  const stopAutoSlide = () => clearInterval(autoSlideInterval);
-
-  currentSlider.addEventListener("mouseover", stopAutoSlide);
-
-  currentSlider.addEventListener("mouseout", startAutoSlide);
-
-  /**
-   * RESPONSIVE
-   */
-
-  window.addEventListener("resize", moveSliderItem);
-
-}
-
-for (let i = 0, len = sliders.length; i < len; i++) { sliderInit(sliders[i]); }
-
-
-
-/**
- * ACCORDION
- */
-
-const accordions = document.querySelectorAll("[data-accordion]");
-
-let lastActiveAccordion;
-
-const accordionInit = function (currentAccordion) {
-
-  const accordionBtn = currentAccordion.querySelector("[data-accordion-btn]");
-
-  accordionBtn.addEventListener("click", function () {
-
-    if (currentAccordion.classList.contains("active")) {
-      currentAccordion.classList.toggle("active");
-    } else {
-      if (lastActiveAccordion) lastActiveAccordion.classList.remove("active");
-      currentAccordion.classList.add("active");
-    }
-
-    lastActiveAccordion = currentAccordion;
-
-  });
-
-}
-
-for (let i = 0, len = accordions.length; i < len; i++) { accordionInit(accordions[i]); }
